5f571a24b6a6541fc51e8a549f7b5422ea33a515
10840130 <71428109+10840130@users.noreply.github.com>
2021-01-09 06:13:05 +0800

    Add files via upload

--- assets/js/animatead.js
+++ assets/js/animatead.js
@@ -2,11 +2,11 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  changeimg(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  changeimg(slideIndex = n);
 }
 
 function showSlides(n) {
@@ -23,16 +23,42 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  /*當自動輪播超出總照片數回到最初的照片 */
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1
-  }
+
   /*替換classname來顯示圖片*/
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 4000) //輪播間隔時間
+  /*自動撥放圖片 */
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  setTimeout(showSlides, 2000);
+}
+
+/*避免btn導致++衝突 */
+function changeimg(n) {
+  var i;
+  var slides = document.getElementsByClassName("slideshow");
+  var dots = document.getElementsByClassName("selection");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  /*遮蔽非當前圖片 */
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  /*替換classname來顯示圖片*/
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+
 }
